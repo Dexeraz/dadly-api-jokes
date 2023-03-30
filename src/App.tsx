3a136eeb48da463adbd88b68228e696b3db32064
Michał Bilik <michal.bilik@amica.com.pl>
2023-03-30 12:51:40 +0200

    fetchRandomJokes moved to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AddJokeForm from './components/AddJokeForm';
 import JokeList from './components/JokeList';
 import { Joke } from './types/Joke';
+import { fetchRandomJokes } from './api/api';
 
 const App: React.FC = () => {
   const [jokes, setJokes] = useState<Joke[]>([]);
+
+  useEffect(() => {
+    fetchRandomJokes(5).then((fetchedJokes) => setJokes(fetchedJokes));
+  }, []);
 
   const handleAddJoke = (joke: Joke) => {
     setJokes((prevJokes) => [...prevJokes, joke]);
