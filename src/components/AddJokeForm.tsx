65a83d259408c9bfa5c959f884c86bce21664356
MBilik <dexeraz@gmail.com>
2023-03-31 07:59:24 +0200

    uuid added for unique jokes ID

--- src/components/AddJokeForm.tsx
+++ src/components/AddJokeForm.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Joke } from "../types/Joke";
+import { v4 as uuidv4 } from 'uuid';
 
 interface AddJokeFormProps {
   onAddJoke: (joke: Joke) => void;
@@ -21,10 +22,10 @@ const AddJokeForm: React.FC<AddJokeFormProps> = ({ onAddJoke }) => {
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     if (joke.joke.trim() === "") {
-      alert("You have found a secret joke! \nWhy do QA engineers hate nature? Because there are too many 🐛! ");
+      alert("You have found a secret joke! \nWhy do QA engineers hate nature? Because there are too many 🐛!");
       return;
     }
-    onAddJoke(joke);
+    onAddJoke({ ...joke, id: uuidv4() });
     setJoke({
       id: "",
       joke: "",
@@ -41,7 +42,7 @@ const AddJokeForm: React.FC<AddJokeFormProps> = ({ onAddJoke }) => {
           placeholder="Enter your own joke"
           className="w-96 p-2 mr-2 rounded border border-gray-300 dark:border-gray-600 dark:bg-gray-800 focus:outline-none focus:border-blue-300 dark:focus:border-blue-500"
         />
-        <button type="submit" className="ml-auto whitespace-no-wrap ">
+        <button type="submit" className="ml-auto whitespace-no-wrap">
           Add Joke
         </button>
       </div>
